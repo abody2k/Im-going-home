eb7157a2b925e358e9d6aa69320b377e5901414e
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-11-26 09:04:35 +0300

    auto destroying the bullets after 3 seconds of no contact

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 var player_position_set = false
-const SPEED = 5.0
+const SPEED = 115.0
 
 func set_player_position(pos : Vector3):
 	look_at(pos)
@@ -15,3 +15,8 @@ func _physics_process(delta):
 			
 			col.get_collider().call("got_attacked")
 			queue_free()
+
+
+func _on_destruction_timeout():
+	queue_free()
+	pass # Replace with function body.
