e5c454cd44c1d67f90377b22847027ece09c2da4
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-11-27 11:05:34 +0300

    added the compass, distance to planet, and added some sprites

--- scripts/spaceship.gd
+++ scripts/spaceship.gd
@@ -3,23 +3,38 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
-
+var fuel = 100.0
+var compass : TextureRect
+var dist : RichTextLabel
+var planet : AnimatableBody2D
+var fuel_ui
+func _ready():
+	fuel_ui = get_parent().get_node("CanvasLayer2/Control/fuel")
+	compass = (get_parent().get_node("CanvasLayer2/Control/circle/compass") as TextureRect)
+	dist = get_parent().get_node("CanvasLayer2/Control/RichTextLabel")
+	planet = get_parent().get_node("AnimatableBody2D")
+	
+var temp_pos : Vector2
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	temp_pos = (planet.global_position - global_position)
+	compass.rotation_degrees = rad_to_deg(atan2(temp_pos.y,temp_pos.x))+90
+	dist.text = "Contact distance : " + str(global_position.distance_squared_to(planet.global_position)-50000)
+	
+	if fuel > 0:
+		if Input.is_action_pressed("action"):
+			velocity= transform.x * SPEED
+			fuel -= 2 * delta
+			fuel_ui.value-=2 * delta
+		else:
+			fuel_ui.value-= delta
+			fuel -= delta
+		
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction = Input.get_axis("left", "right")
+	rotate(deg_to_rad(direction * delta*30))
 
 	move_and_slide()
